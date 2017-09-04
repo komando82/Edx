60da931fcae76e9d1a00716b09a256951cfad1b0
Ivan Neradovic <i.neradovic@levi9.com>
2017-09-04 16:29:00 +0200

    adding Homework 3 code

--- SD4x-Programming-for-the-Web-with-JavaScript/Homework-3/calc.js
+++ SD4x-Programming-for-the-Web-with-JavaScript/Homework-3/calc.js
@@ -2,82 +2,81 @@
  * Implement all your JavaScript in this file!
  */
 
-var display = $('#display');
-
-var button0 = $('#button0'),
-    button1 = $('#button1'),
-    button2 = $('#button2'),
-    button3 = $('#button3'),
-    button4 = $('#button4'),
-    button5 = $('#button5'),
-    button6 = $('#button6'),
-    button7 = $('#button7'),
-    button8 = $('#button8'),
-    button9 = $('#button9');
-
-var addButton = $('#addButton'),
-    subtractButton = $('#subtractButton'),
-    multiplyButton = $('#multiplyButton'),
-    divideButton = $('#divideButton');
-
-var clearButton = $('#clearButton');
-
-var equalsButton = $('#equalsButton');
+var display = $('#display'); // eslint-disable-line 
 
 var resetDisplay = false;
+var opClicked = false;
+var eqClicked = false;
 
 var op = '';
-
 var previousNum = 0;
 var currentNum = 0;
 
-
-
+/**
+ * This function collects the current number in display
+ * 
+ * @return value on the display
+ */
 function checkDisplayValue()
 {
     return display.val();
 }
 
-function result(op, previousNum, currentNum)
+/**
+ * This function should calculate result of operation
+ * 
+ * @return the result of mathematical operation of previous and current number
+ */
+function result()
 {
     if (previousNum === 0)
     {
         return currentNum;
     }
 
-    var result;
+    var res = '';
 
-    previousNum = parseInt(previousNum);
-    currentNum = parseInt(currentNum);
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
 
-    switch(op) {
-        case "+":
-            result = previousNum + currentNum;
-            break;
-        case "-":
-            result = previousNum - currentNum;
-            break;
-        case "*":
-            result = previousNum * currentNum;
-            break;
-        case "/":
-            if (currentNum === 0)
-            {
-                result = 'Infinity';
-            }
-            else
-            {
-                result = previousNum / currentNum;
-            }
-            break;
+    switch(op)
+    {
+    case '+':
+        res = previousNum + currentNum;
+        break;
+    case '-':
+        res = previousNum - currentNum;
+        break;
+    case '*':
+        res = previousNum * currentNum;
+        break;
+    case '/':
+        if (currentNum === 0)
+        {
+            res = 'Infinity';
+        }
+        else
+        {
+            res = previousNum / currentNum;
+        }
+        break;
     }
 
-    return result;
+    return res;
 }
 
 
-$('#button0, #button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9').click(function(){
-    var clickedNum = $(this).val();
+$('#button0, #button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9').click(function() { // eslint-disable-line 
+    var clickedNum = $(this).val(); // eslint-disable-line 
+
+    opClicked = false;
+    eqClicked = false;
+
+    if(eqClicked)
+    {
+        previousNum = 0;
+    }
+
     if(resetDisplay)
     {
         display.val(clickedNum);
@@ -87,52 +86,69 @@ $('#button0, #button1, #button2, #button3, #button4, #button5, #button6, #button
     {
         display.val(checkDisplayValue() + clickedNum);
     }
-    console.log(clickedNum);
 });
 
-addButton.click(function(){
+$('#addButton, #subtractButton, #multiplyButton, #divideButton').click(function() { // eslint-disable-line 
+    var sign = $(this).text(); // eslint-disable-line 
+
+    if(sign !== '+' && sign !== '-' && sign !== '*')
+    {
+        sign = '/';
+    }
+
+    if(eqClicked)
+    {
+        previousNum = 0;
+    }
+
     currentNum = checkDisplayValue();
+
+    if (opClicked)
+    {
+        previousNum = currentNum;
+    }
+    else
+    {
+        previousNum = result();
+    }
+
+    opClicked = true;
     resetDisplay = true;
-    previousNum = result(op, previousNum, currentNum);
-    op = '+';
-    console.log("+");
+    eqClicked = false;
+
+    op = sign;
     display.val(previousNum);
 });
 
-subtractButton.click(function(){
-    currentNum = checkDisplayValue();
-    resetDisplay = true;
-    previousNum = result(op, previousNum, currentNum);
-    op = '-';
-    console.log("-");
-    display.val(previousNum);
+$('#equalsButton').click(function() { // eslint-disable-line 
+    if(opClicked)
+    {
+        return;
+    }
+
+    if(eqClicked)
+    {
+        previousNum = result();
+        display.val(result());
+    }
+    else
+    {
+        currentNum = checkDisplayValue();
+        resetDisplay = true;
+        eqClicked = true;
+
+        display.val(result());
+    }
 });
 
-multiplyButton.click(function(){
-    currentNum = checkDisplayValue();
-    resetDisplay = true;
-    previousNum = result(op, previousNum, currentNum);
-    op = '*';
-    console.log("*");
-    display.val(previousNum);
-});
+$('#clearButton').click(function() { // eslint-disable-line 
+    resetDisplay = false;
 
-divideButton.click(function(){
-    currentNum = checkDisplayValue();
-    resetDisplay = true;
-    previousNum = result(op, previousNum, currentNum);
-    op = '/';
-    console.log("/");
-    display.val(previousNum);
-});
+    op = '';
+    opClicked = false;
 
+    previousNum = 0;
+    currentNum = 0;
 
-
-equalsButton.click(function(){
-    currentNum = checkDisplayValue();
-    resetDisplay = true;
-    console.log("=");
-    console.log(previousNum);
-    console.log(currentNum);
-    display.val(result(op, previousNum, currentNum));
+    display.val('');
 });
